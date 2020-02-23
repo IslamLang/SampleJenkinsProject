5a7727c6b67c08fb4a21ea4f158b6eb58ae6e9b0
IslamLang <56479275+IslamLang@users.noreply.github.com>
2020-02-23 14:18:57 -0500

    secondTest

--- src/main/java/SimpleTest.java
+++ src/main/java/SimpleTest.java
@@ -2,8 +2,12 @@ import org.junit.jupiter.api.Test;
 
 public class SimpleTest {
     @Test
-    public void passingTest(){
+    public void passingTest() {
         System.out.println("This test will always pass!");
     }
-}
 
+    @Test
+    public void secondTest() {
+        System.out.println("never failing test");
+    }
+}
